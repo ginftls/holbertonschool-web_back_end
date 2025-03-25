9e5bf5e0b725a8f226f8be2422785dff522e12d4
ginftls <scdr1905@gmail.com>
2025-03-25 15:39:37 -0400

    Refactor initializeRooms function to accept dynamic student sizes and improve classroom creation

--- ES6_classes/1-make_classrooms.js
+++ ES6_classes/1-make_classrooms.js
@@ -1,10 +1,9 @@
-import ClassRoom from './0-classroom.js';
+import ClassRoom from './0-classroom';
 
-export default function initializeRooms() {
-  // Create an array of ClassRoom objects with the specified sizes
-  return [
-    new ClassRoom(19),
-    new ClassRoom(20),
-    new ClassRoom(34),
-  ];
+export default function initializeRooms(maxStudents = [19, 20, 34]) {
+  const classRooms = [];
+  for (const maxStudent of maxStudents) {
+    classRooms.push(new ClassRoom(maxStudent));
+  }
+  return classRooms;
 }
