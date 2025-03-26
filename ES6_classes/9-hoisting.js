6ffb2d630e291533b18ccdb91e1aa925e97a7450
ginftls <scdr1905@gmail.com>
2025-03-26 08:10:42 -0400

    Change student list export from named to default export for consistency

--- ES6_classes/9-hoisting.js
+++ ES6_classes/9-hoisting.js
@@ -14,20 +14,19 @@ export class HolbertonClass {
   }
 }
 
-// Create instances of HolbertonClass
-const class2019 = new HolbertonClass(2019, 'San Francisco');
-const class2020 = new HolbertonClass(2020, 'San Francisco');
-
-// Define the StudentHolberton class
 export class StudentHolberton {
   constructor(firstName, lastName, holbertonClass) {
     this._firstName = firstName;
     this._lastName = lastName;
-    this._holbertonClass = holbertonClass; // Assign the HolbertonClass instance
+    this._holbertonClass = holbertonClass;
   }
 
   get fullName() {
     return `${this._firstName} ${this._lastName}`;
+  }
+
+  get holbertonClass() {
+    return this._holbertonClass;
   }
 
   get fullStudentDescription() {
@@ -35,12 +34,15 @@ export class StudentHolberton {
   }
 }
 
-// Create instances of StudentHolberton
+const class2019 = new HolbertonClass(2019, 'San Francisco');
+const class2020 = new HolbertonClass(2020, 'San Francisco');
+
 const student1 = new StudentHolberton('Guillaume', 'Salva', class2020);
 const student2 = new StudentHolberton('John', 'Doe', class2020);
 const student3 = new StudentHolberton('Albert', 'Clinton', class2019);
 const student4 = new StudentHolberton('Donald', 'Bush', class2019);
 const student5 = new StudentHolberton('Jason', 'Sandler', class2019);
 
-// Export the list of students as a named export
 export const listOfStudents = [student1, student2, student3, student4, student5];
+
+export default listOfStudents;
