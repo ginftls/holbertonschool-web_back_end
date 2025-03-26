409464834b0de42bcf63fb67312441f50872c46e
ginftls <scdr1905@gmail.com>
2025-03-26 04:41:11 -0400

    Refactor HolbertonCourse class to improve validation for attributes and remove redundant methods

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -1,59 +1,51 @@
+/* eslint-disable */
 export default class HolbertonCourse {
   constructor(name, length, students) {
-    // Validate and assign attributes
-    this._name = this._validateString(name, 'Name');
-    this._length = this._validateNumber(length, 'Length');
-    this._students = this._validateArray(students, 'Students');
+      if (typeof name !== 'string') {
+          throw new TypeError('Name must be a string');
+      }
+      if (typeof length !== 'number') {
+          throw new TypeError('Length must be a number');
+      }
+      if (!Array.isArray(students) || !students.every(student => typeof student === 'string')) {
+          throw new TypeError('Students must be an array of strings');
+      }
+
+      this._name = name;
+      this._length = length;
+      this._students = students;
   }
 
-  // Getter and Setter for name
   get name() {
-    return this._name;
+      return this._name;
   }
 
-  set name(value) {
-    this._name = this._validateString(value, 'Name');
+  set name(newName) {
+      if (typeof newName !== 'string') {
+          throw new TypeError('Name must be a string');
+      }
+      this._name = newName;
   }
 
-  // Getter and Setter for length
   get length() {
-    return this._length;
+      return this._length;
   }
 
-  set length(value) {
-    this._length = this._validateNumber(value, 'Length');
+  set length(newLength) {
+      if (typeof newLength !== 'number') {
+          throw new TypeError('Length must be a number');
+      }
+      this._length = newLength;
   }
 
-  // Getter and Setter for students
   get students() {
-    return this._students;
+      return this._students;
   }
 
-  set students(value) {
-    this._students = this._validateArray(value, 'Students');
-  }
-
-  // Helper method to validate strings
-  _validateString(value, attributeName) {
-    if (typeof value !== 'string') {
-      throw new TypeError(`${attributeName} must be a string`);
-    }
-    return value;
-  }
-
-  // Helper method to validate numbers
-  _validateNumber(value, attributeName) {
-    if (typeof value !== 'number') {
-      throw new TypeError(`${attributeName} must be a number`);
-    }
-    return value;
-  }
-
-  // Helper method to validate arrays of strings
-  _validateArray(value, attributeName) {
-    if (!Array.isArray(value) || !value.every(item => typeof item === 'string')) {
-      throw new TypeError(`${attributeName} must be an array of strings`);
-    }
-    return value;
+  set students(newStudents) {
+      if (!Array.isArray(newStudents) || !newStudents.every(student => typeof student === 'string')) {
+          throw new TypeError('Students must be an array of strings');
+      }
+      this._students = newStudents;
   }
 }
