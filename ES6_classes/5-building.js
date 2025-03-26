c33f082f55dbc30e4da0592a7f2de33b69ec7bbe
ginftls <scdr1905@gmail.com>
2025-03-26 05:38:39 -0400

    Refactor Building class to enforce abstract method implementation and improve sqft property handling

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,19 +1,19 @@
+/*eslint-disable*/
 export default class Building {
-  // Constructor to initialize the attributes
   constructor(sqft) {
-    if (this.constructor === Building) {
-      throw new Error('Building is an abstract class and cannot be instantiated directly');
+    this._sqft = sqft;
+
+    if (this.constructor !== Building && this.evacuationWarningMessage
+      === Building.prototype.evacuationWarningMessage) {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
     }
-    this._sqft = sqft; // Store sqft in _sqft
   }
 
-  // Getter for sqft
   get sqft() {
     return this._sqft;
   }
 
-  // Abstract method evacuationWarningMessage
-  evacuationWarningMessage() {
-    throw new Error('Class extending Building must override evacuationWarningMessage');
+  set sqft(newSqft) {
+    this._sqft = newSqft;
   }
 }
