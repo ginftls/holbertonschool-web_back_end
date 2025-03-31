15ac3eea81dbd4e024c54552d5121ddac8013ca5
ginftls <scdr1905@gmail.com>
2025-03-31 07:36:29 -0400

    Add hasArrayValues function to check if all elements in an array exist in a Set

---
+++ ES6_data_manipulation/7-has_array_values.js
@@ -0,0 +1,11 @@
+export default function hasArrayValues(set, array) {
+  if (!(set instanceof Set)) {
+    throw new TypeError('First argument must be a Set');
+  }
+  
+  if (!Array.isArray(array)) {
+    throw new TypeError('Second argument must be an array');
+  }
+
+  return array.every((element) => set.has(element));
+}
