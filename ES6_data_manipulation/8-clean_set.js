ed9df4ed974c4ca383815884545dd885a4897acd
ginftls <scdr1905@gmail.com>
2025-03-31 08:09:06 -0400

    Refactor cleanSet function to improve performance and simplify string filtering logic

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,18 +1,13 @@
+/*eslint-disable*/
 export default function cleanSet(set, startString) {
-  if (!(set instanceof Set)) {
-    throw new TypeError('First argument must be a Set');
-  }
-
-  if (typeof startString !== 'string') {
-    throw new TypeError('Second argument must be a string');
-  }
-
-  if (startString === '') {
+  if (typeof startString !== 'string' || startString.length === 0) {
     return '';
   }
-
-  return Array.from(set)
-    .filter((value) => typeof value === 'string' && value.startsWith(startString))
-    .map((value) => value.slice(startString.length))
-    .join('_');
+  let result = '';
+  for (const value of set) {
+    if (value.startsWith(startString)) {
+      result += (result ? '-' : '') + value.slice(startString.length);
+    }
+  }
+  return result;
 }
