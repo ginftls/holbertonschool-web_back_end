d9cbc535929c8346cd1f430d860a009fdaf8e283
ginftls <scdr1905@gmail.com>
2025-03-31 09:50:01 -0400

    Fix formatting in groceriesList function for consistency

--- ES6_data_manipulation/9-groceries_list.js
+++ ES6_data_manipulation/9-groceries_list.js
@@ -1,12 +1,11 @@
-/*eslint-disable */
+/*eslint-disable*/
 export default function groceriesList() {
-   const groceries = new Map();
-
-    groceries.set('Apples', 10);
-    groceries.set('Tomatoes', 10);
-    groceries.set('Pasta', 1);
-    groceries.set('Rice', 1);
-    groceries.set('Bananas', 5);
+  const groceries = new Map();
+  groceries.set('Apples', 10);
+  groceries.set('Tomatoes', 10);
+  groceries.set('Pasta', 1);
+  groceries.set('Rice', 1);
+  groceries.set('Bananas', 5);
 
    return groceries;
 }
