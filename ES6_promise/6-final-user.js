93687ad3f6f343c75340eebcc5dacb20a92aea3d
ginftls <scdr1905@gmail.com>
2025-04-01 03:54:48 -0400

    Refactor handleProfileSignup to use arrow function syntax and streamline promise result mapping

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,23 +1,9 @@
 /* eslint-disable */
-import signUpUser from './4-user-promise.js';
-import uploadPhoto from './5-photo-reject.js';
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((results) => {
-      return results.map((result) => {
-        if (result.status === 'fulfilled') {
-          return {
-            status: 'fulfilled',
-            value: result.value,
-          };
-        } else {
-          // Extract only the error message for rejected promises
-          return {
-            status: 'rejected',
-            value: result.reason.message,
-          };
-        }
-      });
-    });
-}
+export default (firstName, lastName, fileName) => Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+  .then((results) => results.map((promise) => ({
+    status: promise.status,
+    value: promise.status === 'fulfilled' ? promise.value : `${promise.reason.name}: ${promise.reason.message}`,
+  })));
