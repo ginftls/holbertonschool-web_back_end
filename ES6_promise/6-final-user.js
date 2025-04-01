36a28f49f1fab1db6a5753673bea051d0d358d07
ginftls <scdr1905@gmail.com>
2025-04-01 03:38:56 -0400

    Refactor handleProfileSignup to import signUpUser and uploadPhoto for improved functionality

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,5 +1,6 @@
 /* eslint-disable */
-import { uploadPhoto, createUser } from './utils.js';
+import signUpUser from './4-user-promise.js';
+import uploadPhoto from './5-photo-reject.js';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   const promises = [
