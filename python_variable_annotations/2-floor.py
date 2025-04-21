0c6f324a50fe2a749778238ed24fe575aa6ba5c1
ginftls <scdr1905@gmail.com>
2025-04-21 08:23:47 -0400

    Add implementation of floor function to compute the floor of a float with type annotations

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Module for computing the floor of a float with type annotations."""
+
+
+import math
+
+
+def floor(n: float) -> int:
+    """Return the floor of the given float."""
+    return math.floor(n)
---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Module for converting a float to its string representation w/ annotations"""
+
+
+def to_str(n: float) -> str:
+    """Return the string representation of the given float."""
+    return str(n)
