d08c36bf7e73df573cfcc1759aa4879ad626b604
ginftls <scdr1905@gmail.com>
2025-04-21 10:30:49 -0400

    Add implementation of make_multiplier function to create a multiplier function with type annotations

---
+++ python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,7 @@
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    def multiplier_func(value: float) -> float:
+        return value * multiplier
+    return multiplier_func
