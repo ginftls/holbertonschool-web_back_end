d81bfaa7bc3ddfcbea1564ae7e74212e35bc03e7
ginftls <scdr1905@gmail.com>
2025-04-21 10:37:44 -0400

    Add docstring to 8-make_multiplier.py for module and function clarity

--- python_variable_annotations/8-make_multiplier.py
+++ python_variable_annotations/8-make_multiplier.py
@@ -1,8 +1,14 @@
 #!/usr/bin/env python3
+"""
+This module defines the make_multiplier function that returns
+a function to multiply a float by a given multiplier.
+"""
+
 from typing import Callable
 
 
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """Returns a function that multiplies a float by the given multiplier."""
     def multiplier_func(value: float) -> float:
         return value * multiplier
     return multiplier_func
