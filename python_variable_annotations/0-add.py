dcd12a63fc04f10991f203664b33bea64a909029
ginftls <scdr1905@gmail.com>
2025-04-21 07:02:53 -0400

    pycodestyle

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """Module for adding two floats with type annotations."""
 
+
 def add(a: float, b: float) -> float:
     """Adds two floats and returns their sum as a float."""
     return a + b
