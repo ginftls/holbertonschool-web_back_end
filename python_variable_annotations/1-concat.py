8f1527adb2c11b2c670ec943a03e96246a48e839
ginftls <scdr1905@gmail.com>
2025-04-21 07:05:15 -0400

    Add implementation of concat function for concatenating two strings with type annotations

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Module for concatenating two strings with type annotations."""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Concatenates two strings and returns the result as a string."""
+    return str1 + str2
