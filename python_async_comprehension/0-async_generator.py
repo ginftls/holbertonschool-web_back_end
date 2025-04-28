8e27c323eb42806cb99c95a98867a7c8835e095e
ginftls <scdr1905@gmail.com>
2025-04-28 04:59:23 -0400

    Fix type hint in async_generator function to use AsyncGenerator

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 """Module for an asynchronous generator that yields random numbers."""
 
-from typing import AsyncIterator
+from typing import AsyncGenerator
 import asyncio
 import random
 
 
-async def async_generator() -> AsyncIterator[float]:
+async def async_generator() -> AsyncGenerator[float, None]:
     """Asynchronously yields random numbers between 0 and 10 ten times."""
     for _ in range(10):
         await asyncio.sleep(1)
